075c59a11cc6e069a009023af46d59be895ffb80
alastor-lan <71885079+alastor-lan@users.noreply.github.com>
2020-10-17 00:32:39 +0800

    UITextDemo
    include Turntext、win&lose text、button size；

---
+++ Assets/AITurn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AITurn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject aiTurnUI;
+    private float alpha = 0.0f;
+    private float alphaSpeed = 2.0f;
+    private CanvasGroup cg;
+    public void Awake()
+    {
+        cg = this.transform.GetComponent<CanvasGroup>();
+        GameManager.instance.ePlayerTurnEnd.AddListener(AITurntext);
+        gameObject.SetActive(false);
+
+    }
+    void Update()
+    {
+        
+        if (alpha != cg.alpha)
+        {
+            cg.alpha = Mathf.Lerp(cg.alpha, alpha, alphaSpeed * Time.deltaTime);
+            if (Mathf.Abs(alpha - cg.alpha) <= 0.01)
+            {
+                cg.alpha = alpha;                            
+            }
+        }
+       
+    }
+
+    public void Hide()
+    {
+        alpha = 0;
+
+    }
+    void AITurntext()
+    {
+        Hide();
+        gameObject.SetActive(true);
+        Debug.Log("电脑回合，text改变");
+        cg.alpha = 1;    
+    }
+
+}
---
+++ Assets/LoseDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseDisplay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject loseUI;
+  
+   
+    public void Awake()
+    {
+        GameManager.instance.eGameLose.AddListener(Losetext);
+        gameObject.SetActive(false);
+    }
+    
+    void Losetext()
+    {
+      
+       loseUI.SetActive(true);
+
+        Debug.Log("你输了");
+    }
+
+}
+
