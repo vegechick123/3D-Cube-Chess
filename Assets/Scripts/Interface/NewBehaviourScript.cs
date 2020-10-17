d65706f625fdfbca9be90f3299e48bfd704073f5
vegechick123 <destiny173@163.com>
2020-10-17 23:14:24 +0800

    test

---
+++ Assets/Scripts/Interface/NewBehaviourScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatTextFactory:MonoBehaviour
+{
+    public static GameObject CreatFloatText(Vector3 position)
+    {
+        Debug.Log(position);
+        return null;
+    }
+}
---
+++ Assets/Scripts/Interface/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public FloatTextFactory target;
+    public Vector3 t;
+    private void Awake()
+    {
+        target.C
+    }
+}
